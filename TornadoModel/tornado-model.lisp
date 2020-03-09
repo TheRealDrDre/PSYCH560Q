c61b35d0f74101d3707cd8ed98a40d9955e9a4ea
gala-rose <36901008+gala-rose@users.noreply.github.com>
2020-03-09 12:42:28 -0700

    Update tornado-model.lisp
    -removed transfer of stimulus chunk to imaginal. Not part of problem state (imaginal buffer) until make-decision state is deciding. changed retrieval of magnitude to start from visual buffer (as opposed to imaginal). Add "mag" slot to magnitude. 
    
    -Added outcome-learned to make-decision chunk. considered removing decision-made and only used outcome-learned. But because there will be 7 forecast updates eventually, there will be times when a decision is made but participant must see more forecasts before outcome is learned.

--- TornadoModel/tornado-model.lisp
+++ TornadoModel/tornado-model.lisp
@@ -1,21 +1,22 @@
-;;tornado model
+;;;tornado model
 (clear-all)
 
 (define-model tornado-model
 
-;; SET PARAMETERS
+;;; SET PARAMETERS
 
-;;---------------------------------------
-;; chunk types
-;;---------------------------------------
+;;;---------------------------------------
+;;; chunk types
+;;;---------------------------------------
 
-;; Chunk to keep track task in goal module.
-(chunk-type goal
-	state ;;look-screen, encoding, translating
-	decision ;;yes/no. whether a decision to shelter or not shelter was made or not. "Yes" will be a requirement to begin next trial.
+;;; Chunk to keep track of task in goal module.
+(chunk-type make-decision
+	state ;;look-screen, encoding, translating, deciding
+	decision-made ;;yes/no. whether a decision to shelter or not shelter was made or not. "Yes" will be a requirement to begin next trial.
+	outcome-learned ;;yes/no. whether the interface has said if a tornado hit or not.
 	)
 
-;; stimulus chunk is information about the forecast taken from the visual buffer.
+;;; stimulus chunk is information about the forecast taken from the visual buffer.
 (chunk-type stimulus
 	forecast
 	number
@@ -23,23 +24,28 @@
 	word
 	)
 
-;; This chunk represents a magnitude (preloaded - think of as real-world knowledge) associated with colors, numbers, and words.
+;;; This chunk represents a magnitude (preloaded - think of as real-world knowledge) associated with colors, numbers, and words.
 (chunk-type magnitude ;;represents a position on the gradient from 1 to 10 magnitude
 	mag
-	;;number 
-	;;color 
-	;;word
+	number 
+	color 
+	word
 	)
-
-(chunk-type make-decision ;;consider combining with first chunk type?
-            state)
-
-;;---------------------------------------
-;; preload chunks
-;;---------------------------------------
-;;set stimulus chunk to green to start
+	
+;;; After a decision is made (and all forecasts have been seen) the interface gives a tornado outcome. Consider including point balance in this outcome for future. 
+(chunk-type instance
+	decision ;;shelter/not shelter
+	tornado-hit ;;yes/no
+	mag ;;should this be mag and/or the stimulus visual characteristics?
+	)
+	
+	    
+;;;---------------------------------------
+;;; preload chunks
+;;;--------------------------------------
+;;;set stimulus chunk to green to start
 (add-dm (first-forcast ISA stimulus color green)
-;;create the starting goal state as look-screen
+;;;create the starting goal state as look-screen
 		(first-decision ISA make-decision
                         state look-screen)
         (look-screen);;must create the chunk that will be in slot of first decision (or will get a warning)
@@ -47,53 +53,20 @@
 	(translating))
 		;;(mag-green-hi ISA magnitude color green mag 3)) ;;considering making 2 or three chunks per color (hi, med, weak strength)? 
 
-;;place stimulus in visual buffer
+;;;place stimulus in visual buffer
 (set-buffer-chunk 'visual 'first-forcast)
 
-;; set the initial goal
+;;; set the initial goal
 (goal-focus first-decision)
 
-;;---------------------------------------
-;; productions
-;;---------------------------------------
+;;;---------------------------------------
+;;;productions
+;;;production names are always the root form of the verb (e.g. decide, translate). State of task will always be present participle of the production in progress (e.g. translating, deciding). 
+;;;---------------------------------------
 
-;; Imaginal buffer holds current problem state information
-;; production encode-stimulus takes information from visual buffer and adds to imaginal
-(P encode-stimulus
-	=visual>
-	  isa stimulus
-	  forecast =cur_forecast
-	  color =color1
-	  number =num1 ;;how to address num2? (e.g. the "40%"" in the "25-40%") 
-	  word =word1
-
-	?visual>
-	  state free
-
-   	?imaginal>
-   	  state free
-   	  buffer empty
-
-    =goal>
-      ISA make-decision
-      state look-screen
-
-==> 
-	*goal>
-	  ISA 	make-decision
-	  state	encoding		
-
-   +imaginal>
-   	 isa	stimulus
-   	 forecast =cur_forecast
- 	 color =color1
-	 number =num1
-	 word =word1
-   	) 
-
-;; production translate takes info from imaginal buffer and retrieves a magnitude based on associative strength.
+;;; production translate takes info from visual buffer and retrieves a magnitude based on associative strength.
 (p translate
-	=imaginal>
+	=visual>
 	  isa	stimulus
    	  forecast =cur_forecast
  	  color =color1
@@ -102,27 +75,70 @@
 
 	?imaginal>
    	  state free
+   	  buffer empty
 
 	=goal>
 	  ISA make-decision
 	  state encoding
+   	  decision-made no
+   	  outcome-learned no
 
 	?retrieval>
-      state free
-      buffer empty
+      	  state free
+     	  buffer empty
 
  ==>  
  	*goal>
  	  ISA make-decision
  	  state translating
-
-    +retrieval> 
-      ISA magnitude
+   	  decision-made no
+   	  outcome-learned no
+	
+   	+retrieval> 
+      	  ISA magnitude
+   	  mag =mag1
    	  color =color1
    	  number =num1
    	  word =word1
-   	;;let imaginal clear?
-	)
+	
+   	+imaginal>
+   	  ISA magnitude
+   	  mag =mag1
+   )
+	
+;;; productions take magnitude and make decision based on shelter/not shelter threshold. s is Shelter. n is not shelter.
+(p decide-s
+	=goal>
+	  ISA make-decision
+	  state translating
+	  decision-made no
+   	  outcome-learned no
+   	
+   	=imaginal>
+   	  state free
+   
+   	=imaginal>
+     	  ISA magnitude
+   	  mag1 >= 2.5 ;;can i directly compute here?
+   	  decision nil
+   	
+   	?manual>
+     	  preparation free
+          processor   free
+    	  execution   free
+   
+   ==>  
+   	*goal>
+ 	  ISA make-decision
+ 	  state deciding
+   	  decision-made yes
+   	  outcome-learned no
+   
+   	*imaginal>
+    	  decision =shelter
 
-;; production takes magnitude and makes decision based on shelter/not shelter threshold
+    	+manual>
+     	  cmd press-key
+   	  key =s
 )
+	
